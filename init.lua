2a1ac6f1738478e769d8a24be433a1bd9a4c7fbe
rainx0r <angelos.h@hotmail.com>
2024-07-28 18:40:46 +0100

    Add vim-tmux-navigator

--- init.lua
+++ init.lua
@@ -443,6 +443,23 @@ require('lazy').setup({
       require('nvim-treesitter.configs').setup(opts)
     end,
   },
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
 
   -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
