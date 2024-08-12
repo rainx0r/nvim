87fbf86fc4f72ae497acdee7c69766aae9ed10ec
rainx0r <angelos.h@hotmail.com>
2024-08-12 10:05:38 +0100

    Add split binds

--- init.lua
+++ init.lua
@@ -29,6 +29,9 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>el', '<cmd>vsplit<CR>')
+vim.keymap.set('n', '<leader>ej', '<cmd>split<CR>')
+vim.keymap.set('n', '<C-x>', '<cmd>q<CR>')
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
