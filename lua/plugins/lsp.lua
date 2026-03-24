c38d9be0f08128ac0aa9d7d6693412e953909618
rain <evan@latent.dev>
2026-03-24 18:31:33 +0000

    Fix treesitter config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -35,7 +35,7 @@ return {
     config = function(_, opts)
       require('nvim-treesitter.install').prefer_git = true
       ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+      require('nvim-treesitter.config').setup(opts)
     end,
   },
   {
